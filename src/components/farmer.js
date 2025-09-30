c7fb26df503d2e21cbbdb510b9a13abec97bcbd0
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2025-09-30 21:18:12 +0600

    frontend

--- src/components/farmer.js
+++ src/components/farmer.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
-const farmer = () => {
+const farmer = ({active}) => {
   return (
-    <div>
-      
-    </div>
+    <section className={`content-section ${active ? 'active' : ''} bg-white rounded-2xl shadow-lg p-6`}>
+        <h2 className="text-2xl font-bold mb-4 text-green-700">👨‍🌾 Farmer Section</h2>
+        <form className='grid grid-cols-1 md:grid-cols-6 gap-6'>
+            
+        </form> 
+    </section>
   )
 }
 
