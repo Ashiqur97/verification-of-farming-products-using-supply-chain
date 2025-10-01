fc006c88c3706121d658751c44f59f731fa03b6c
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2025-10-01 23:16:46 +0600

    frontend

--- src/components/farmer.js
+++ src/components/farmer.js
@@ -4,8 +4,12 @@ const farmer = ({active}) => {
   return (
     <section className={`content-section ${active ? 'active' : ''} bg-white rounded-2xl shadow-lg p-6`}>
         <h2 className="text-2xl font-bold mb-4 text-green-700">👨‍🌾 Farmer Section</h2>
-        <form className='grid grid-cols-1 md:grid-cols-6 gap-6'>
-            
+          <form className="grid grid-cols-1 md:grid-cols-2 gap-6">
+        <div>
+          <label className="block text-gray-700 font-semibold">Batch ID</label>
+          <input type="text" placeholder="RICE-2025-1001"
+                 className="w-full border rounded-lg px-4 py-2 mt-1 focus:ring-2 focus:ring-green-400" />
+        </div>
         </form> 
     </section>
   )
