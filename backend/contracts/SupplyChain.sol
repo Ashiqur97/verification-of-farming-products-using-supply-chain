272df65c87f5906d304ae01412ae24746738dfde
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2025-10-08 14:14:34 +0600

    updating

--- backend/contracts/SupplyChain.sol
+++ backend/contracts/SupplyChain.sol
@@ -1,13 +1,12 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract SupplyChain is AccessControl {
+contract supplychain is AccessControl {
     uint256 private _batchIds;
-    
-    // Define roles
-    bytes32 public constant FARMER_ROLE = keccak256("FARMER_ROLE");
+
+    bytes32 public constant FARMER_ROLE = keccak256("FAMER_ROLE");
     bytes32 public constant DISTRIBUTOR_ROLE = keccak256("DISTRIBUTOR_ROLE");
     bytes32 public constant RETAILER_ROLE = keccak256("RETAILER_ROLE");
     bytes32 public constant CONSUMER_ROLE = keccak256("CONSUMER_ROLE");
@@ -50,12 +49,27 @@ contract SupplyChain is AccessControl {
         bool certificationVerified;
     }
 
-    mapping(uint256 => Batch) public batches;
-    mapping(string => uint256) public batchIdToIndex;
 
-      event BatchCreated(uint256 indexed batchId, address indexed farmer, string batchIdentifier);
+    mapping (uint256 => Batch) public batches;
+    mapping (string => uint256) public batchToIndex;
+
+    event BatchCreated(uint256 indexed batchId, address indexed farmer, string batchIdentifier);
     event BatchTransit(uint256 indexed batchId, address indexed from, address indexed distributor);
     event Processed(uint256 indexed batchId, address indexed distributor);
     event BatchPackaged(uint256 indexed batchId,address indexed retailer);
+    event BatchForSale(uint256 indexed batchId, address indexed retailer);
+    event BatchForSold(uint256 indexed batchId, address indexed consumer);
+    event BatchRecalled(uint256 indexed batchId,address indexed initiator,string reason);
+    event PackagingDetailsUpdated(uint256 indexed batchId, uint256 packagingDate, string storageConditions);
+    event RetailDetailsUpdated(uint256 indexed batchId, uint256 arrivalDate, uint256 stockQuantity, uint256 sellingPrice);
+    event CertificationVerified(uint256 indexed batchId, bool verified);
 
+
+    constructor() {
+        _grantRole(DEFAULT_ADMIN_ROLE,msg.sender);
+        _grantRole(ADMIN_ROLE,msg.sender);
+    }
+
+    
+    
 }
