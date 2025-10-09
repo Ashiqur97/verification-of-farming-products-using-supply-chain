60fb452b8b4509c3147a34a8ad79d54e57cb6780
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2025-10-09 13:33:35 +0600

    updating

--- backend/contracts/SupplyChain.sol
+++ backend/contracts/SupplyChain.sol
@@ -51,7 +51,7 @@ contract supplychain is AccessControl {
 
 
     mapping (uint256 => Batch) public batches;
-    mapping (string => uint256) public batchToIndex;
+    mapping (string => uint256) public batchIdToIndex;
 
     event BatchCreated(uint256 indexed batchId, address indexed farmer, string batchIdentifier);
     event BatchTransit(uint256 indexed batchId, address indexed from, address indexed distributor);
@@ -70,6 +70,69 @@ contract supplychain is AccessControl {
         _grantRole(ADMIN_ROLE,msg.sender);
     }
 
-    
-    
+      function addFarmer(address account) public onlyRole(ADMIN_ROLE) {
+        _grantRole(FARMER_ROLE, account);
+    }
+
+    function addDistributor(address account) public onlyRole(ADMIN_ROLE) {
+        _grantRole(DISTRIBUTOR_ROLE, account);
+    }
+
+    function addRetailer(address account) public onlyRole(ADMIN_ROLE) {
+        _grantRole(RETAILER_ROLE, account);
+    }
+
+    function addConsumer(address account) public onlyRole(ADMIN_ROLE) {
+        _grantRole(CONSUMER_ROLE, account);
+    }
+
+    function CreateBatch(
+        string memory _batchId,
+        string memory _certificateId,
+        string memory _crop,
+        string memory _origin,
+        uint256  _harvestDate,
+        string memory _quality,
+        uint256 _weight,
+        string memory _storageConditions,
+        string memory _destination,
+        string memory _farmerName
+    ) public onlyRole(FARMER_ROLE) {
+        require(batchIdToIndex[_batchId] == 0, "Batch ID already exists");
+
+        _batchIds++;
+        uint256 newBatchId = _batchIds;
+
+        batches[newBatchId] = Batch({
+            id: newBatchId,
+            batchId: _batchId,
+            certificateId: _certificateId,
+            crop:_crop,
+            origin:_origin,
+            harvestDate: _harvestDate,
+            farmer: msg.sender,
+            distributor: address(0),
+            retailer: address(0),
+            consumer: address(0),
+            weight: _weight,
+            quality: _quality,
+            status: BatchStatus.Created,
+            createdAt: block.timestamp,
+            updatedAt: block.timestamp,
+            storageConditions: _storageConditions,
+            destination: _destination,
+            farmerName : _farmerName,
+            retailerName: "",
+            packagingDate:0,
+            arrivalDate:0,
+            stockQuantity:0,
+            sellingPrice:0,
+            certificationVerified:false
+        });
+
+        batchIdToIndex[_batchId] = newBatchId;
+        emit BatchCreated(newBatchId,msg.sender,_batchId);
+
+    }
+ 
 }
